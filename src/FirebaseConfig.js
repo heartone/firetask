eaa3b8caeadd9f506ee8e8aa07678d0116f1adb7
原宏之 <heartone@mba.local>
2022-09-09 18:56:39 +0900

    プロジェクト名変更＆再デプロイ

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyD2gKU0Uyc5syArTEJsNK-OBuC0Iny-cNk",
-  authDomain: "taskapp-8101.firebaseapp.com",
-  databaseURL: "https://taskapp-8101-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "taskapp-8101",
-  storageBucket: "taskapp-8101.appspot.com",
-  messagingSenderId: "92743115609",
-  appId: "1:92743115609:web:ebcb336f0713281c88a248",
-  measurementId: "G-BPEDWVZ5D5"
+  apiKey: "AIzaSyAR5zMwikcJR1X-EaTuaac_ujx6MRQaHgY",
+  authDomain: "firetask-8101.firebaseapp.com",
+  projectId: "firetask-8101",
+  databaseURL: "https://firetask-8101-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  storageBucket: "firetask-8101.appspot.com",
+  messagingSenderId: "42513117461",
+  appId: "1:42513117461:web:7985a4a8c6b312e0a4b91c",
+  measurementId: "G-XNFJ4K30Y7"
 };
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
