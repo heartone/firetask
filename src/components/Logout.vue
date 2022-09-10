0b1292c57fa418d32fc1dc472fc3ac27bac9646a
原宏之 <heartone@mba.local>
2022-09-10 18:53:36 +0900

    デプロイ設定

---
+++ src/components/Logout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue"
+import { getAuth, signOut } from "firebase/auth"
+import { useAppStore } from "@/stores/app.js"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+const auth = getAuth()
+const appStore = useAppStore()
+const isLoading = ref(false)
+const logout = async () => {
+  isLoading.value = true
+  try {
+    await signOut(auth)
+    router.push("/login")
+  } catch(error) {
+    console.log(error)
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
+<template>
+  <div v-if="appStore.currentUser">
+    <button class="btn-sm" @click="logout()"><i class="fa fa-sign-out-alt"></i> ログアウト</button>
+  </div>
+</template>
